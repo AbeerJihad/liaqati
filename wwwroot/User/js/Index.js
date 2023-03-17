f42b3e77e2536385fbc615ed27d098265e85403b
Omar Mohammad Hamad <108229755+EngOmarHamad@users.noreply.github.com>
2023-03-17 18:43:47 +0200

    test1

---
+++ wwwroot/User/js/Index.js
@@ -0,0 +1,7 @@
+// var navbarContainer = document.querySelector(".navbar-container");
+// window.onscroll = () => {
+//   if (window.innerWidth > 992) {
+//     navbarContainer.classList.toggle("bg-white", window.scrollY > 30);
+//     navbarContainer.classList.toggle("shadow", window.scrollY > 30);
+//   }
+// };
